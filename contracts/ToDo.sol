13ddb9b9492e463d41d497235be1e45d8a376a79
AbelOsaretin <contact.abel321@gmail.com>
2024-02-12 23:24:48 +0100

    Created ToDo.sol file

---
+++ contracts/ToDo.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ToDo {
+
+    TODO: Create title for todo task
+    TODO: Create description for todo task
+    TODO: Create status for todo task
+    TODO: Get all todo created
+    TODO: Update a todo task
+    TODO: Delete a todo task
+    TODO: Toggle status task
+
+    
+}
